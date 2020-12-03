6772507fb4cd349f3e01eb1fbb5c32175ee2d7e8
shindepragati <60525827+shindepragati@users.noreply.github.com>
2020-12-02 22:30:58 -0800

    Add files via upload

--- finalproject/src/main/java/edu/sjsu/cmpe275/finalproject/model/User.java
+++ finalproject/src/main/java/edu/sjsu/cmpe275/finalproject/model/User.java
@@ -1,44 +1,57 @@
-/*
- * package edu.sjsu.cmpe275.finalproject.model;
- * 
- * import javax.persistence.Column; import javax.persistence.Entity; import
- * javax.persistence.GeneratedValue; import javax.persistence.GenerationType;
- * import javax.persistence.Id; import javax.persistence.Table;
- * 
- * @Entity
- * 
- * @Table(name = "user") public class User { public User() {
- * 
- * }
- * 
- * public User(String userName, String nickName) { this.userName = userName;
- * this.nickName = nickName; }
- * 
- * public long getId() { return id; }
- * 
- * public void setId(long id) { this.id = id; }
- * 
- * public String getUserName() { return userName; }
- * 
- * public void setUserName(String userName) { this.userName = userName; }
- * 
- * public String getNickName() { return nickName; }
- * 
- * public void setNickName(String nickName) { this.nickName = nickName; }
- * 
- * @Id
- * 
- * @GeneratedValue(strategy = GenerationType.AUTO) private long id;
- * 
- * //@Id
- * 
- * @Column private String userName;
- * 
- * public String getName() { return name; }
- * 
- * public void setName(String name) { this.name = name; }
- * 
- * @Column private String name;
- * 
- * @Column private String nickName; }
- */
+
+package edu.sjsu.cmpe275.finalproject.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+	public User() {
+
+	}
+
+	@Id
+	@Column(name = "user_name")
+	private String userName;
+
+	@Column(name = "nick_name")
+	private String nickName;
+
+//	@OneToMany(mappedBy = "user")
+//	private List<BankAccount> bankaccounts;
+
+	public User(String userName, String nickName) {
+		this.userName = userName;
+		this.nickName = nickName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+//	public List<BankAccount> getBankaccounts() {
+//		return bankaccounts;
+//	}
+//
+//	public void setBankaccounts(List<BankAccount> bankaccounts) {
+//		this.bankaccounts = bankaccounts;
+//	}
+}
