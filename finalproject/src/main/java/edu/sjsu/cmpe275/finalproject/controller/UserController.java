85a64dbb871ca9331aa76cbd856c8228c74b373a
shindepragati <60525827+shindepragati@users.noreply.github.com>
2020-12-02 22:30:24 -0800

    Add files via upload

--- finalproject/src/main/java/edu/sjsu/cmpe275/finalproject/controller/UserController.java
+++ finalproject/src/main/java/edu/sjsu/cmpe275/finalproject/controller/UserController.java
@@ -1,43 +1,38 @@
-/*
- * package edu.sjsu.cmpe275.finalproject.controller;
- * 
- * import org.springframework.beans.factory.annotation.Autowired; import
- * org.springframework.http.HttpStatus; import
- * org.springframework.http.ResponseEntity; import
- * org.springframework.web.bind.annotation.CrossOrigin; import
- * org.springframework.web.bind.annotation.PostMapping; import
- * org.springframework.web.bind.annotation.RequestMapping; import
- * org.springframework.web.bind.annotation.RequestParam; import
- * org.springframework.web.bind.annotation.RestController;
- * 
- * import edu.sjsu.cmpe275.finalproject.model.User; import
- * edu.sjsu.cmpe275.finalproject.services.UserService;
- * 
- * @CrossOrigin(origins = "*")
- * 
- * @RestController
- * 
- * @RequestMapping("/user") public class UserController {
- * 
- * @Autowired UserService userService;
- * 
- * @PostMapping("") public ResponseEntity<User>
- * createBankAccount(@RequestParam(required = true, name = "userName") String
- * userName,
- * 
- * @RequestParam(required = true, name = "nickName") String nickName){
- * 
- *//**
-	 * postman
-	 * 
-	 * http://localhost:9091/bank?userName=sindhu
-	 *//*
-		 * 
-		 * try { //Address address = new Address(street, city, state, zip);
-		 * //BankAccount bankAccount = new BankAccount(bankName, country, address,
-		 * ownerName, currency, sendingOrReceiving); User user = new User(userName,
-		 * nickName); User _user = userService.saveUser(user); return new
-		 * ResponseEntity<User>(_user, HttpStatus.OK); } catch (Exception e) {
-		 * System.err.println(e); return new ResponseEntity<>(null,
-		 * HttpStatus.BAD_REQUEST); } } }
-		 */
+
+package edu.sjsu.cmpe275.finalproject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.sjsu.cmpe275.finalproject.model.User;
+import edu.sjsu.cmpe275.finalproject.services.UserService;
+
+@CrossOrigin(origins = "*")
+
+@RestController
+
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+	UserService userService;
+
+	@PostMapping("/adduser")
+	public ResponseEntity<User> createBankAccount(@RequestBody User user) {
+
+		try {
+
+			User _user = userService.saveUser(user);
+			return new ResponseEntity<User>(_user, HttpStatus.OK);
+		} catch (Exception e) {
+			System.err.println(e);
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		}
+	}
+}
